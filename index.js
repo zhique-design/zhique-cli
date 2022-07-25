2a0d4d60aac077a1cdf9a121246cb30ad65fc81e
xuzhao <twtyjvkg@foxmail.com>
2022-07-25 15:57:31 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+const program = require('commander');
+const download = require('download-git-repo');
+
+program.version('1.0.0', '-v --version')
+    .command('new <projectName>')
+    .action((projectName) => {
+        console.log('clone template ...');
+        download('github:zhique-design/zhique-front-template', projectName, function (err) {
+            console.log(err ? 'Error' : 'Success')
+        })
+    });
+program.parse(process.argv);
